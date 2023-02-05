4ef2e87a1ddba5bce6b4519433aeaf443cad9a2a
surshreya <surshreya.0727@gmail.com>
2023-02-05 10:21:04 +0530

    test suite setup

--- test/header.test.js
+++ test/header.test.js
@@ -2,12 +2,20 @@ const puppeteer = require("puppeteer");
 
 jest.setTimeout(30000);
 
-test("Launch a browser", async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto("localhost:3000");
+let browser, page;
 
+beforeEach(async () => {
+  browser = await puppeteer.launch({ headless: false });
+  page = await browser.newPage();
+  await page.goto("localhost:3000");
+});
+
+test("Launch a browser", async () => {
   // TEST THE HEADER
   const appText = await page.$eval("a.brand-logo", (el) => el.innerHTML);
   expect(appText).toEqual("Blogster");
+});
+
+afterEach(async () => {
+  await browser.close();
 });
